6a8641f9a80fc7bb43397ab728a2b49dfe1bd5b3
Christophe Blonde <warlutin@mbpdechristophe.home>
2021-07-15 00:04:18 +0200

    update-request-backend

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,12 +14,31 @@ const Task = (props) => {
       x.checkBox === y.checkBox ? 0 : x.checkBox ? 1 : -1
     );
     /* Send change in BDD */
-    console.log(arrTaskChecked[index]);
+    console.log(arrTaskChecked[index].id);
+    axios.put(
+      `http://localhost:5000/task/update/${arrTaskChecked[index].id}`,
+      arrTaskChecked[index],
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
     return setSubmit(arrTaskChecked);
   };
 
   const handleDeleteTask = (index) => {
     const arr = [...submit];
+    console.log(arr);
+    /* Delete in BDD */
+    const token =
+      "iNC47naNC041BhR0ZW0xzCpDmwUiio1VwGPCmwI5GKBINEfVTNjMDMhciRGlTOtM";
+    axios.delete(
+      `http://localhost:5000/task/delete/${arr[index].id}`,
+
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+      arr[index]
+    );
     /* Delete Task */
     arr.splice(index, 1);
     return setSubmit(arr);
